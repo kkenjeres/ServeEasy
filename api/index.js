ba2c1acf0aeecaf6df3c6de361f5e085025184d0
kkenjeres <kkenjeres96@gmail.com>
2023-04-11 23:32:41 +0200

    back

--- api/index.js
+++ api/index.js
@@ -1,56 +1,56 @@
-const express = require("express");
-const cors = require("cors");
-const escpos = require("escpos");
+// const express = require("express");
+// const cors = require("cors");
+// const escpos = require("escpos");
 
-const app = express();
-const port = 3001;
+// const app = express();
+// const port = 3001;
 
-const PRINTER = {
-  device_name: "Epson TM-M30",
-  host: "192.168.2.113",
-  port: 9100,
-};
+// const PRINTER = {
+//   device_name: "Epson TM-M30",
+//   host: "192.168.2.113",
+//   port: 9100,
+// };
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(express.json());
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(express.json());
 
-app.post("/print", async (req, res) => {
-  const { text } = req.body;
+// app.post("/print", async (req, res) => {
+//   const { text } = req.body;
 
-  try {
-    const device = new escpos.Network(PRINTER.host, PRINTER.port);
-    const printer = new escpos.Printer(device);
+//   try {
+//     const device = new escpos.Network(PRINTER.host, PRINTER.port);
+//     const printer = new escpos.Printer(device);
 
-    device.open(async (error) => {
-      if (error) {
-        console.error("Ошибка при подключении к устройству:", error);
-        res.status(500).json({ error: "Не удалось подключиться к устройству" });
-        return;
-      }
+//     device.open(async (error) => {
+//       if (error) {
+//         console.error("Ошибка при подключении к устройству:", error);
+//         res.status(500).json({ error: "Не удалось подключиться к устройству" });
+//         return;
+//       }
 
-      try {
-        printer
-          .align("CT")
-          .text(text)
-          .cut()
-          .close();
+//       try {
+//         printer
+//           .align("CT")
+//           .text(text)
+//           .cut()
+//           .close();
 
-        console.log("Текст успешно напечатан!");
-        res.status(200).json({ message: "Текст успешно напечатан" });
-      } catch (error) {
-        console.error("Ошибка печати:", error);
-        res.status(500).json({ error: "Ошибка печати" });
-      } finally {
-        device.close();
-      }
-    });
-  } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error);
-    res.status(500).json({ error: "Ошибка при выполнении запроса" });
-  }
-});
+//         console.log("Текст успешно напечатан!");
+//         res.status(200).json({ message: "Текст успешно напечатан" });
+//       } catch (error) {
+//         console.error("Ошибка печати:", error);
+//         res.status(500).json({ error: "Ошибка печати" });
+//       } finally {
+//         device.close();
+//       }
+//     });
+//   } catch (error) {
+//     console.error("Ошибка при выполнении запроса:", error);
+//     res.status(500).json({ error: "Ошибка при выполнении запроса" });
+//   }
+// });
 
-app.listen(3001, () => {
-  console.log(`Server is listening at http://localhost:3001`);
-});
+// app.listen(3001, () => {
+//   console.log(`Server is listening at http://localhost:3001`);
+// });
 
--- src/PrintComponent.js
+++ src/PrintComponent.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function PrintComponent() {
-  const [text, setText] = useState("");
-  const [message, setMessage] = useState(null);
-  const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://rest-project.vercel.app/";
+// function PrintComponent() {
+//   const [text, setText] = useState("");
+//   const [message, setMessage] = useState(null);
+//   const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://rest-project.vercel.app/";
 
-  const handlePrint = async () => {
-    try {
-      const response = await fetch(`${apiUrl}/print`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
-      });
+//   const handlePrint = async () => {
+//     try {
+//       const response = await fetch(`${apiUrl}/print`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ text }),
+//       });
   
-      if (response.ok) {
-        setMessage("Текст успешно напечатан!");
-      } else {
-        const errorData = await response.json();
-        console.error("Ошибка печати:", errorData);
-        setMessage("Ошибка печати, пожалуйста, попробуйте еще раз.");
-      }
-    } catch (error) {
-      console.error("Ошибка при выполнении запроса:", error);
-      setMessage("Ошибка печати, пожалуйста, попробуйте еще раз.");
-    }
-  };
+//       if (response.ok) {
+//         setMessage("Текст успешно напечатан!");
+//       } else {
+//         const errorData = await response.json();
+//         console.error("Ошибка печати:", errorData);
+//         setMessage("Ошибка печати, пожалуйста, попробуйте еще раз.");
+//       }
+//     } catch (error) {
+//       console.error("Ошибка при выполнении запроса:", error);
+//       setMessage("Ошибка печати, пожалуйста, попробуйте еще раз.");
+//     }
+//   };
   
 
-  return (
-    <div>
-      <h1>Печать на принтере Epson TM30</h1>
-      <textarea
-        placeholder="Введите текст для печати"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
-      <button onClick={handlePrint}>Печать</button>
-      {message && <p>{message}</p>}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Печать на принтере Epson TM30</h1>
+//       <textarea
+//         placeholder="Введите текст для печати"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       ></textarea>
+//       <button onClick={handlePrint}>Печать</button>
+//       {message && <p>{message}</p>}
+//     </div>
+//   );
+// }
 
-export default PrintComponent;
+// export default PrintComponent;
