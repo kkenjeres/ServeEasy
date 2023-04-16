5e80eaca958dce366c15792451cc8c8c7088967a
kkenjeres <kkenjeres96@gmail.com>
2023-04-16 21:11:53 +0200

    server

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const PRINTER = {
   port: 9100,
 };
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://restaurant-app-opal.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.post("/print", async (req, res) => {
